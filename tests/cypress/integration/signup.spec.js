4f33fc8ea9eca8f4fc1f67795a16bf1d8e703bb5
kikacavalher <ericavalher@gmail.com>
2024-07-09 19:49:26 +0100

    more one context in signup tests

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,9 +1,10 @@
 const { faker } = require('@faker-js/faker');
 
+// it.only(texto) -- para executar somente 1 teste
 //Como se fosse o plano de teste.
 describe('signup', function () {
 
-    //Para preparar um contexto de teste, como user novo, user com compras feitas...
+    //Para preparar um contexto de teste, usars os its como cenarios dentro de um teste. 
     context('quando o user é novo', function () {
         // super constante para otimizar o uso de dados compartilhados
         const user = {
@@ -37,6 +38,50 @@ describe('signup', function () {
         })
     })
 
+    context('quando o user ja existe', function () {
+        const userPost = {
+            email: 'erica.cavalher@gmail.com',
+            is_provider: true,
+            name: 'Erica Cavalher',
+            password: 'Kika1234'
+        }
+
+        before(function () {
+            // Chamar a task que remove o dado no bd (foi criado no arquivo plugins -- index.js)
+            cy.task('removeUser', userPost.email)
+                .then(function (result) {
+                    console.log(result)
+                })
+            // Adicionar um user no banco antes do teste para ter certeza que funciona sem dependencia de outros steps.
+            cy.request(
+                'POST',
+                'http://localhost:3333/users',
+                userPost
+            ).then(function (response) {
+                expect(response.status).to.eq(200)
+            })
+        })
+
+        it('Is not possible add user with the same email', function () {
+            cy.visit('/signup')
+
+            cy.get('input[placeholder="Nome"]').type(userPost.name)
+            cy.get('input[placeholder="E-mail"]').type(userPost.email)
+            cy.get('input[placeholder="Senha"]').type(userPost.password)
+            cy.contains('button[type="submit"]', 'Cadastrar').click()
+
+            cy.get('.toast')
+                .should('be.visible')
+                .find('p')
+                .should('have.text', 'Email já cadastrado para outro usuário.')
+
+            // Pegar o body para ficar mais facil visualizar um elemento
+            // cy.wait(1000)
+            // cy.get('body')
+
+            })
+    })
+
     // Mudando o status da requisição para aceitar e não adicionar no banco. 
     it('Must signup a new randon user and force status 200', function () {
         const email = faker.internet.email()
@@ -63,47 +108,6 @@ describe('signup', function () {
             .should('be.visible')
             .find('p')
             .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
-
-    })
-    // it.only(texto) -- para executar somente 1 teste
-    it('Is not possible add user with the same email', function () {
-
-        const userPost = {
-            email: 'erica.cavalher@gmail.com',
-            is_provider: true,
-            name: 'Erica Cavalher',
-            password: 'Kika1234'
-        }
-
-        cy.task('removeUser', userPost.email)
-            .then(function (result) {
-                console.log(result)
-            })
-
-        // Adicionar um user no banco antes do teste para ter certeza que funciona sem dependencia de outros steps.
-        cy.request(
-            'POST',
-            'http://localhost:3333/users',
-            userPost
-        ).then(function (response) {
-            expect(response.status).to.eq(200)
-        })
-
-        cy.visit('/signup')
-
-        cy.get('input[placeholder="Nome"]').type(userPost.name)
-        cy.get('input[placeholder="E-mail"]').type(userPost.email)
-        cy.get('input[placeholder="Senha"]').type(userPost.password)
-        cy.contains('button[type="submit"]', 'Cadastrar').click()
-
-        cy.get('.toast')
-            .should('be.visible')
-            .find('p')
-            .should('have.text', 'Email já cadastrado para outro usuário.')
-
-        // Pegar o body para ficar mais facil visualizar um elemento
-        // cy.wait(1000)
-        // cy.get('body')
 
     })
 })
