bdca00cd163252d6a91e9558d0d123b6dec46295
kikacavalher <ericavalher@gmail.com>
2024-07-09 19:41:06 +0100

    include context in tests

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,45 +1,53 @@
 const { faker } = require('@faker-js/faker');
 
+//Como se fosse o plano de teste.
 describe('signup', function () {
 
-    // super constante para otimizar o uso de dados compartilhados
-    const user = {
-        name: 'Erica Cavalher',
-        email: 'erica.cavalher@gmail.com',
-        password: 'Kika1234'
-    }
+    //Para preparar um contexto de teste, como user novo, user com compras feitas...
+    context('quando o user é novo', function () {
+        // super constante para otimizar o uso de dados compartilhados
+        const user = {
+            name: 'Erica Cavalher',
+            email: 'erica.cavalher@gmail.com',
+            password: 'Kika1234'
+        }
 
-    it('Must signup remove of db and add new user ', function () {
+        before(function () {
+            // Chamar a task que remove o dado no bd (foi criado no arquivo plugins -- index.js)
+            cy.task('removeUser', user.email)
+                .then(function (result) {
+                    console.log(result)
+                })
+        })
 
-        // Chamar a task que remove o dado no bd (foi criado no arquivo plugins -- index.js)
-        cy.task('removeUser', user.email)
-            .then(function (result) {
-                console.log(result)
-            })
+        it('Must signup remove of db and add new user ', function () {
 
-        cy.visit('/signup')
+            cy.visit('/signup')
 
-        cy.get('input[placeholder="Nome"]').type(user.name)
-        cy.get('input[placeholder="E-mail"]').type(user.email)
-        cy.get('input[placeholder="Senha"]').type(user.password)
-        cy.contains('button[type="submit"]', 'Cadastrar').click()
+            cy.get('input[placeholder="Nome"]').type(user.name)
+            cy.get('input[placeholder="E-mail"]').type(user.email)
+            cy.get('input[placeholder="Senha"]').type(user.password)
+            cy.contains('button[type="submit"]', 'Cadastrar').click()
 
-        cy.get('.toast')
-            .should('be.visible')
-            .find('p')
-            .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+            cy.get('.toast')
+                .should('be.visible')
+                .find('p')
+                .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
 
+        })
     })
 
     // Mudando o status da requisição para aceitar e não adicionar no banco. 
     it('Must signup a new randon user and force status 200', function () {
         const email = faker.internet.email()
+        const name = 'Erica Cavalher'
+        const password = 'Kika1234'
 
         cy.visit('/signup')
 
-        cy.get('input[placeholder="Nome"]').type(user.name)
+        cy.get('input[placeholder="Nome"]').type(name)
         cy.get('input[placeholder="E-mail"]').type(email)
-        cy.get('input[placeholder="Senha"]').type(user.password)
+        cy.get('input[placeholder="Senha"]').type(password)
 
         // Criamos um ouvinte para esperar a req POST e troca para 200
         cy.intercept('POST', '/users', {
@@ -58,7 +66,7 @@ describe('signup', function () {
 
     })
     // it.only(texto) -- para executar somente 1 teste
-    it.only('Is not possible add user with the same email', function () {
+    it('Is not possible add user with the same email', function () {
 
         const userPost = {
             email: 'erica.cavalher@gmail.com',
@@ -68,24 +76,24 @@ describe('signup', function () {
         }
 
         cy.task('removeUser', userPost.email)
-        .then(function (result) {
-            console.log(result)
-        })
+            .then(function (result) {
+                console.log(result)
+            })
 
         // Adicionar um user no banco antes do teste para ter certeza que funciona sem dependencia de outros steps.
         cy.request(
             'POST',
             'http://localhost:3333/users',
             userPost
-        ).then(function(response){
+        ).then(function (response) {
             expect(response.status).to.eq(200)
         })
 
         cy.visit('/signup')
 
-        cy.get('input[placeholder="Nome"]').type(user.name)
-        cy.get('input[placeholder="E-mail"]').type(user.email)
-        cy.get('input[placeholder="Senha"]').type(user.password)
+        cy.get('input[placeholder="Nome"]').type(userPost.name)
+        cy.get('input[placeholder="E-mail"]').type(userPost.email)
+        cy.get('input[placeholder="Senha"]').type(userPost.password)
         cy.contains('button[type="submit"]', 'Cadastrar').click()
 
         cy.get('.toast')
