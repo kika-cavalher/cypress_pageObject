6f43dd050a7dbfd1844f36032aa7e026a72af105
kikacavalher <ericavalher@gmail.com>
2024-07-15 16:51:45 +0100

    Finish first test Login

--- tests/cypress/integration/signin.spec.js
+++ tests/cypress/integration/signin.spec.js
@@ -2,12 +2,18 @@ import signinPage from '../support/pages/signin/index.js'
 import dashPage from '../support/pages/header'
 
 describe('signin', function () {
-    context('quando o user é top das galaxias', function () {
+    context('When the user is top das galaxias', function () {
         const user = {
             name: 'Erica Cavalher',
+            is_provider: true,
             email: 'erica.cavalher@gmail.com',
             password: 'Kika1234'
         }
+
+        // Simplificando a chamada de remover e adicionar o user novamente colocando como comando nativo. 
+        before(function () {
+            cy.postUser(user)
+        })
 
         it('Must login with success ', function () {
             signinPage.goToPage()
@@ -16,6 +22,5 @@ describe('signin', function () {
 
             dashPage.header.userLoggedIn(user.name)
         })
-        
     })
 })
