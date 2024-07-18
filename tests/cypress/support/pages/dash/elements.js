3126e9ef5907f7738387c205af869d3ce8590758
kikacavalher <ericavalher@gmail.com>
2024-07-18 12:32:46 +0100

    exception whew is the last day of the mounth

--- tests/cypress/support/pages/dash/elements.js
+++ tests/cypress/support/pages/dash/elements.js
@@ -1,6 +1,8 @@
 exports.el = {       
     calendar: '.DayPicker',
     today: '.DayPicker-Day--available',
-    appointmentHour: '.appointment'
+    appointmentHour: '.appointment',
+    nextPageCalendar: '.DayPicker-NavButton--next',
+    mounthTitle: '//div[@class="DayPicker-Caption"]'
 
 }
--- tests/cypress/support/pages/dash/index.js
+++ tests/cypress/support/pages/dash/index.js
@@ -1,6 +1,6 @@
 require('cypress-xpath');
 
-import {el} from './elements.js'
+import { el } from './elements.js'
 import signinPage from '../signin/index.js'
 import header from '../../components/header/index.js'
 
@@ -19,11 +19,70 @@ class DashPage {
     }
 
     calendarShouldBeVisible() {
-        cy.get(el.calendar, {timeout: 7000})
+        cy.get(el.calendar, { timeout: 7000 })
             .should('be.visible')
     }
 
     chooseNextDay(day) {
+
+        let today = new Date()
+        //Para pegar o ultimo dia do mes. 
+        let lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0)
+
+        if (today.getDate() === lastDay.getDate()) {
+            cy.get(el.nextPageCalendar)
+                .should('be.visible')
+                .click()
+
+            let now = new Date();
+            // Para sempre escolher o dia de amanha
+            now.setDate(now.getDate() + 1);
+
+            let monthName
+            switch (now.getMonth()) {
+                case 0:
+                    monthName: 'Janeiro'
+                    break
+                case 1:
+                    monthName: 'Fevereiro'
+                    break
+                case 2:
+                    monthName: 'Março'
+                    break
+                case 3:
+                    monthName: 'Abril'
+                    break
+                case 4:
+                    monthName: 'Maio'
+                    break
+                case 5:
+                    monthName: 'Junho'
+                    break
+                case 6:
+                    monthName: 'Julho'
+                    break
+                case 7:
+                    monthName: 'Agosto'
+                    break
+                case 8:
+                    monthName: 'Setembro'
+                    break
+                case 9:
+                    monthName: 'Outubro'
+                    break
+                case 10:
+                    monthName: 'Novembro'
+                    break
+                case 11:
+                    monthName: 'Dezembro'
+                    break
+            }
+
+            //Checkpoint que trocou o calendario.
+            cy.xpath(el.mounthTitle, monthName)
+                .should('be.visible')
+
+        }
 
         // ^ -- começa com
         // $ -- termina com
